ac75edcd08de128fc14fa9f942c8c70dd92afcc5
Jorel Abessolo <jorelabessolo@gmail.com>
2022-12-15 04:02:17 +0100

    LF file modified

--- 0x02-functions_nested_loops/10-main.c
+++ 0x02-functions_nested_loops/10-main.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * main - check the code
  *
@@ -9,7 +8,6 @@
 int main(void)
 {
     int n;
-
     n = add(89, 9);
     printf("%d\n", n);
     return (0);
