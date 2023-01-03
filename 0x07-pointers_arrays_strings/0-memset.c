dd1c889f9be54a92390623c16a0264a29ec07a36
jorel abessolo <jorelabessolo@gmail.com>
2023-01-03 11:04:41 -0800

    task 0 of 0x07

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,18 +2,18 @@
 /**
  * _memset - fill a block of memory with a specific value
  * @s: starting address of memory to be filled
- * @b: the desired value
+ * @c: the desired value
  * @n: number of bytes to be changed
  * Return: changed array with new value for n bytes
  */
-char *_memset(char *s, char b, unsigned int n)
+void *_memset(void *s, int c, size_t n)
 {
-	int i = 0;
+	unsigned int i = 0;
+	unsigned char *memory = s, value = c;
 
-	for (; n > 0; i++)
+	for (i = 0; i < n; i++)
 	{
-		s[i] = b;
-		n--;
+		memory[i] = value;
 	}
-	return (s);
+	return (memory);
 }
