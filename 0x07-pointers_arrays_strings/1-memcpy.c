781fa5d74b4b540fac5e838f2daca97b461b3c23
jorel abessolo <jorelabessolo@gmail.com>
2023-01-03 18:29:03 -0800

    task 1----

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,7 +9,7 @@
  * _memcpy -a function that copies memory area
  * @dest: memory where is stored
  * @src: memory where is copied
- * @n: number of bytes
+ * @n: number of bytes -
  * Return: copied memory with n byted changed
  */
 void *_memcpy(void *dest, const void *src, size_t n)
