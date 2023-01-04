d8b0fe7b745c34c5d880dfc5f4fefa3c5936f2f8
jorel abessolo <jorelabessolo@gmail.com>
2023-01-04 06:38:30 -0800

    task 8---

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -75,4 +75,27 @@ char *postfix_match(char *str, char *postfix)
 }
 
 /**
- * wildcmp - Compares two strings, considering wildcard
+ * wildcmp - Compares two strings, considering wildcad characters.
+ * @S1: The first string to be compared.
+ * @s2: The second string to be compared - may contain wildcards.
+ *
+ * Return: if the strings can be considered identical - 1.
+ * Otherwise -0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*')
+	{
+		iterate_wild(&s2);
+		s2 = postfix_match(s1, s2);
+	}
+	if (*s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s1 != *s2)
+	{
+		return (0);
+	}
+	return (wildcmp(++s1, ++s2));
+}
